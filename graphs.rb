48d93fe75a7e5baf20ecff97e470cf4296f6f827
adamberman <adamberman13@gmail.com>
2015-01-26 20:44:13 -0800

    create edge class

--- graphs.rb
+++ graphs.rb
@@ -6,3 +6,21 @@ class Vertex
 	end
 end
 
+class Edge
+	attr_reader :from_vertex, :to_vertex, :cost
+
+	def initialize(from_vertex, to_vertex, cost = 1)
+		@from_vertex = from_vertex
+		@to_vertex = to_vertex
+		@cost = cost
+		@from_vertex.outward_edges << self
+		@to_vertex.inward_edges << self
+	end
+
+	def destroy
+		@from_vertex.outward_edges.delete(self)
+		@to_vertex.inward_edges.delete(self)
+		@from_vertex = nil
+		@to_vertex = nil
+	end
+end
