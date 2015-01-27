fabe9c112c35e85d0c85f2e2f5416e5958ed83c3
adamberman <adamberman13@gmail.com>
2015-01-26 21:28:38 -0800

    add dijkstras algorithm

--- graphs.rb
+++ graphs.rb
@@ -24,3 +24,37 @@ class Edge
 		@to_vertex = nil
 	end
 end
+
+def dykstras(start_vertex)
+	final_paths = {}
+	possible_paths = {
+		start_vertex => {
+			prev_edge: nil,
+			total_cost: 0
+		}
+	}
+
+	while true
+		min_vertex, min_data = possible_paths.min_by { |_, v| v[:total_cost] }
+		break if min_vertex.nil?
+
+		final_paths[min_vertex] = min_data
+		possible_paths.delete(min_vertex)
+
+		min_vertex.edges.each do |edge|
+			next_vertex = (edge.to_vertex == min_vertex) ? edge.from_vertex : edge.to_vertex
+			next_cost = min_data[:total_cost] + edge.cost
+
+			next_data = possible_paths[:next_vertex]
+
+			unless next_data && next_data[:total_cost] < next_cost
+				possible_paths[next_vertex] = {
+					prev_edge: edge,
+					total_cost: next_cost
+				}
+			end
+		end
+	end
+
+	final_paths
+end
